2eb9b950d670590ecf02e0d1d6fdda75d2cd81b7
Maksim Titiaiev <gipsmas@gmail.com>
2018-12-28 01:22:00 +0200

    это для тестов

--- src/index.js
+++ src/index.js
@@ -6,3 +6,14 @@ const model = new Model();
 
 const view = new View();
 const controller = new Controller(model, view);
+
+// function test() {
+//   document.getElementsByClassName('day')[0].dataset.key = '29/12/2018/4';
+//   model.setEventOfDay({ key: '27/12/2018/4', event: [{ id: Date.now(), title: 'header', body: 'some info' }] });
+//   console.log('27/12/2018/4', model.getAllEventsOfDay('27/12/2018/4'));
+//   // model.deleteEventOfDayById('29/12/2018/4', 1545951105469);
+//   model.updateEventOfDayById('29/12/2018/4', 1545951875938, { qw: 3, members: 'qwewrrqwr' });
+//   console.log(model.getEventOfDayById('29/12/2018/4', 1545951875938));
+// }
+
+// test();
