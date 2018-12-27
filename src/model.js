be6da69a78986752e029d47d0f7dcc8fa366e1aa
Maksim Titiaiev <gipsmas@gmail.com>
2018-12-27 22:52:03 +0200

    подрефакторил новые методы модели

--- src/model.js
+++ src/model.js
@@ -1,13 +1,15 @@
+/* eslint-disable class-methods-use-this */
 import { EventEmitter } from './helpers';
 
 class Model extends EventEmitter {
   constructor(items = []) {
     super();
     this.items = items;
+
     // FIXME: позже удалить проверку ниже
     // для проверки
-  //   this.setDayInfo({ key: '27/12/2018/4', data: 'some info', title: 'header' });
-  //   console.log(this.getDayInfo('27/12/2018/4'));
+    this.setEventsOfDay({ key: '27/12/2018/4', events: [{ title: 'header', body: 'some info' }] });
+    console.log(this.getAllEventsOfDay('27/12/2018/4'));
   }
 
   addItem(item) {
@@ -16,34 +18,55 @@ class Model extends EventEmitter {
   }
 
   // FIXME: разобраться с емитером
-  // Метод получения данных по дню
-  // Принимает строку вида "27/12/2018/4"
-  // Возвращает обеъкт с данными или null
-  // eslint-disable-next-line class-methods-use-this
-  getDayInfo(requestString) {
-    if (typeof requestString !== 'string') throw new RangeError('getDayInfo(responseString) - аргумент должен быть строкового типа!');
+
+  /**
+   * Метод получения событий дня
+   *
+   * Принимает строку вида "27/12/2018/4"
+   *
+   * @param {string} queryString
+   * @returns {array} Возвращает массив событий или null
+   * @memberof Model
+   */
+  getAllEventsOfDay(queryString) {
+    if (typeof queryString !== 'string') {
+      console.error('queryString', queryString);
+      throw new TypeError('getAllEventsOfDay(queryString) - аргумент должен быть строкового типа!');
+    }
+    if (queryString.split('/').length !== 4) {
+      console.error('queryString', queryString);
+      throw new Error('getAllEventsOfDay(queryString) - недостаточно данных в строке, аргумент должен иметь такой формат "27/12/2018/4"');
+    }
     if (localStorage) {
-      const info = JSON.parse(localStorage.getItem(requestString));
-      if (info) {
-        return info;
+      const events = JSON.parse(localStorage.getItem(queryString));
+      if (events) {
+        return events;
       }
     }
     return null;
   }
 
-  // Метод добавления информации о дне в хранилище
-  // принимает объект с обязательным свойством key значение которого строка вида "27/12/2018/4"
-  // eslint-disable-next-line class-methods-use-this
-  setDayInfo(obj) {
+  /**
+ * Метод добавления событий дня в хранилище
+ *
+ * @param {object}  принимает объект такого вида { key: '27/12/2018/4', events: [ ] }
+ * @memberof Model
+ */
+  setEventsOfDay(obj = { key: '', events: [] }) {
     if (!obj.key || typeof obj.key !== 'string') {
-      throw new Error('setDayInfo(obj) - obj обязательно должен иметь свойство key с строковым значением вида "27/12/2018/4"');
+      throw new Error('setEventsOfDay(obj) - obj обязательно должен иметь свойство key с строковым значением вида "27/12/2018/4"');
+    }
+
+    if (!obj.events) {
+      throw new Error('setEventsOfDay(obj) - obj обязательно должен иметь свойство events содержащее массив объектов событий');
     }
 
     if (localStorage) {
       const { key } = obj;
       // eslint-disable-next-line no-param-reassign
       delete obj.key;
-      localStorage.setItem(key, JSON.stringify(obj));
+      const { events } = obj;
+      localStorage.setItem(key, JSON.stringify(events));
     }
   }
 }
