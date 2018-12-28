24e6d6e15b7d5dbd121327203e055d85cac14ffa
Olegato <oleg.egunkov@yandex.ru>
2018-12-28 11:48:39 +0300

    Добавил data-key атрибуты

--- src/view.js
+++ src/view.js
@@ -86,13 +86,13 @@ class View extends EventEmitter {
       for(let  i = 0; i < 6; i++) calendar += '<td>';
     }
     for(let  i = 1; i <= Dlast; i++) {
-      if (i == new Date().getDate() && D.getFullYear() == new Date().getFullYear() && D.getMonth() == new Date().getMonth()) {
-        calendar += `<td class="today" [data-key="${i}"]>'${i} `; 
-      }else{
-        let day = new Date(D.getFullYear(),D.getMonth(),i).getDay();
+      let day = new Date(D.getFullYear(),D.getMonth(),i).getDay();
         if(day == 0){
           day = 7;
-        } 
+        }
+      if (i == new Date().getDate() && D.getFullYear() == new Date().getFullYear() && D.getMonth() == new Date().getMonth()) {
+        calendar += `<td class="day today" [data-key="${i}/${D.getMonth()}/${D.getFullYear()}/${day}"]>${i}`; 
+      }else{
         calendar += `<td class="day" [data-key="${i}/${D.getMonth()}/${D.getFullYear()}/${day}"]>${i}`;
       }
       if (new Date(D.getFullYear(),D.getMonth(),i).getDay() == 0) {
